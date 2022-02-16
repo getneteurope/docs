50b30b25832a6a67673563d990c506998ace1ed3
Liviu Coman <liviu.coman@getneteurope.com>
2022-02-16 15:51:27 +0100

    PSPDOC-1786: support invalidation of cache

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-if [[ -z ${AWS_S3_BUCKET} || -z ${AWS_S3_FOLDER} ]]; then
-  echo 'Must speciy environment variables: AWS_S3_BUCKET, AWS_S3_FOLDER'
+if [[ -z ${AWS_S3_BUCKET} || -z ${AWS_S3_FOLDER} || -z ${AWS_CF_DIST_ID} ]]; then
+  echo 'Must speciy environment variables: AWS_S3_BUCKET, AWS_S3_FOLDER, AWS_CF_DIST_ID'
   echo "AWS_S3_BUCKET: ${AWS_S3_BUCKET}"
   echo "AWS_S3_FOLDER: ${AWS_S3_FOLDER}"
+  echo "AWS_CF_DIST_ID: ${AWS_CF_DIST_ID}"
   echo 'Deployment skipped'
   exit 1
 fi
 
-aws s3 sync "public/" "s3://${AWS_S3_BUCKET}/${AWS_S3_FOLDER}/"
+aws s3 sync "public/" "s3://${AWS_S3_BUCKET}/${AWS_S3_FOLDER}/" --delete
+aws cloudfront create-invalidation --distribution-id ${AWS_CF_DIST_ID} --paths "/*"
